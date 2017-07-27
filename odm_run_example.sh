1ec3d59dc2d1f1568f9aa53870b041b1af00ac7a
Matthew Berryman <matthew@acrossthecloud.net>
2017-07-27 18:09:51 +1000

    fixes #7

--- odm_run_example.sh
+++ odm_run_example.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 bash odm_runner.sh odm_boruszyn_kap
+sleep 360
 bash odm_runner.sh odm_data_aukerman
+sleep 360
 bash odm_runner.sh odm_data_seneca
+sleep 360
 bash odm_runner.sh odm_data_waterbury
