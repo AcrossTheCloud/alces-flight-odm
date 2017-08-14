0b50440393c883712418d827dbd119b4797b4095
Matthew Berryman <matthew@acrossthecloud.net>
2017-08-14 18:43:34 +1000

    changes for AWS PS Summit demo

--- odm_run_example.sh
+++ odm_run_example.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-/opt/clusterware/bin/alces customize slave add feature/configure-docker
-eval `/opt/clusterware/bin/alces module load services/pdsh`
-pdsh -g nodes "alces customize apply feature/configure-docker"
 bash odm_runner.sh odm_boruszyn_kap
-sleep 360
 bash odm_runner.sh odm_data_aukerman
-sleep 360
 bash odm_runner.sh odm_data_seneca
-sleep 360
 bash odm_runner.sh odm_data_waterbury
