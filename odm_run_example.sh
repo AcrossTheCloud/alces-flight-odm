b4b9c1833c68fc410b1a9abacc3ae1f711dbb8f3
Matthew Berryman <matthew@acrossthecloud.net>
2017-08-17 12:02:00 +1000

    fixes #9

--- odm_run_example.sh
+++ odm_run_example.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+s3cmd get --recursive s3://acrossthecloud-example-s3/odm-input-data/ .
 bash odm_runner.sh odm_boruszyn_kap
 bash odm_runner.sh odm_data_aukerman
 bash odm_runner.sh odm_data_seneca
