4a26bf64ef5e65607ac44d02d2c3686a3f131dc3
Matthew Berryman <matthew@acrossthecloud.net>
2017-08-22 08:40:59 +1000

    don't specify storage name (set it as default in s3 setup)

--- odm_run_example.sh
+++ odm_run_example.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-alces storage get -n alcesdemo --recursive s3://acrossthecloud-example-s3/odm-input-data/ .
+alces storage get --recursive s3://acrossthecloud-example-s3/odm-input-data/ .
 bash odm_runner.sh odm_boruszyn_kap
 bash odm_runner.sh odm_data_aukerman
 bash odm_runner.sh odm_data_seneca
